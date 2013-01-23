a7a0cc65eafbda049dc5eb05734efbf394854fea
Andriy <arudyk.dev@gmail.com>
2013-01-22 19:19:08 -0500

    modularizing code

--- wcu_balance.pl
+++ wcu_balance.pl
@@ -11,55 +11,70 @@
 #
 
 use strict;
-use WWW::Mechanize;
-use WWW::Mechanize::TreeBuilder;
-use HTTP::Cookies;
 use Crypt::SSLeay;
+use HTTP::Cookies;
+use HTML::TreeBuilder;
+use Term::ReadKey;
+use WWW::Mechanize;
 
-my $num_args = $#ARGV + 1;
-if ($num_args != 2) {
-    print "usage: ./wcu_balance.pl <92-number> <pin>\n";
-    exit;
+my $usage_string = "usage: ./wcu_balance.pl <92-number>\n";
+my $mycat_url    = "http://www.wcu.edu/11407.asp";
+
+sub check_args {
+    my $num_args = $#ARGV + 1;
+    if ($num_args != 1 || $ARGV[0] !~ m/92/) {
+        print $usage_string;
+        print "Make sure the value you entered is a valid WCU 92 number!\n";
+        exit;
+    }
 }
 
-my $mech = WWW::Mechanize->new();
-WWW::Mechanize::TreeBuilder->meta->apply($mech);
-$mech->cookie_jar(HTTP::Cookies->new());
-$mech->agent('Mozilla/5.0');
-$mech->get("http://www.wcu.edu/11407.asp");
+sub read_passwd {
+    print "Enter your MyCat pin: ";
+    ReadModel('noecho'); # dont echo password
+    chomp(my $pass = <STDIN>);
+    ReadModel(0);        # switch back to normal
+    
+    return $pass;
+}
 
-my $result=$mech->submit_form(
+sub check_settings {
+    print "stub";
+}
+
+sub login_get_page {
+    my $user_agent = WWW::Mechanize->new();
+    $user_agent->cookie_jar(HTTP::Cookies->new());
+    $user_agent->agent('Mozilla/5.0');
+    $user_agent->get("http://www.wcu.edu/11407.asp");
+
+    my $page = $user_agent->submit_form(
         form_number => 3,
-        fields      => {
-            id    => $ARGV[0],
-            PIN  => $ARGV[1]
+        fields => {
+            id => $_[0],
+            PIN => $_[1]
         },
-        button     => 'submit'
+        button => 'submit'
     );
-#sleep(4);
-#print $result->content();
 
-my @rray = $mech->find('strong');
-
-while (my $token = shift @rray) {
-    if ($token->as_text() =~ "Board Meals") {
-        $token = shift @rray;
-        print "Board Meals:" . $token->as_text() . "\n";
-    }
-    if ($token->as_text() =~ "Meal Points") {
-        $token = shift @rray;
-        print "Meal Points: " . $token->as_text() . "\n";
-    }
-    if ($token->as_text() =~ "Cat Cash") {
-        $token = shift @rray;
-        print "Cat Cash:    " . $token->as_text() . "\n";
-    }
+    return $page->content();
 }
 
-#foreach (@rray) {
-#    if ($_->as_text() =~ "Board Meals") {
-#        print "board meals found";
-#    }
-#    #print $_->as_text();
-#    print "\n"
-#}
+sub parse_page {
+    my $tree = HTML::TreeBuilder->new();
+    $tree->parse_content($_[0]);
+    my @table_items = $tree->look_down('_tag' => 'strong');
+
+    shift @table_items;
+    foreach (@table_items) {
+        print $_->content_list;
+        print "\n";
+    }
+
+    $tree->delete;
+}
+
+my $us = $ARGV[0];
+my $pa = $ARGV[1];
+my $con = login_get_page($us, $pa);
+parse_page($con);
